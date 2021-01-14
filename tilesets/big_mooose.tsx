e1cd602d77ed37c71cfda25e7b23c9318666ff2b
PhilippBehmer <kontakt@philippbehmer.de>
2021-01-14 11:55:12 +0000

    Deploying to gh-pages from master @ d28702e8c937b560693b5092c4c9439feab1749b 🚀

---
+++ tilesets/big_mooose.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="big_mooose" tilewidth="32" tileheight="32" tilecount="120" columns="12">
+ <image source="big_mooose.png" trans="ffffff" width="410" height="320"/>
+</tileset>
---
+++ tilesets/water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="water" tilewidth="32" tileheight="32" tilecount="3072" columns="64">
+ <image source="water.png" trans="000000" width="2048" height="1536"/>
+</tileset>
